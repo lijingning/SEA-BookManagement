058f052654fae26f72ce451f7175460bc600cfb4
1368315257 <1368315257@qq.com>
2018-03-02 14:01:15 +0800

    MODELS

---
+++ BookManagement/Models/Borrow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BookManagement.Models
+{
+    public class Borrow
+    {
+        public int ID { get; set; }
+        public int BookID { get; set; }
+        public int PersonID { get; set; }
+        public string BorrowDate { get; set; }
+        public string ReturnDate { get; set; }
+        public bool Subscribe { get; set; }
+        public string SubscribeDate { get; set; }
+        public Book Book = new Book();
+        public Person Person = new Person();
+    }
+    public class BorrowDBContext:DbContext
+    {
+        public BorrowDBContext()
+        {
+
+        }
+        public DbSet<Borrow> Borrow { get; set; }
+    }
+
+}
